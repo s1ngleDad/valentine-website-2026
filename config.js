151bae2b5171c513aaed3cab40447885eebda06f
Славьк <136058983+s1ngleDad@users.noreply.github.com>
2026-02-04 10:11:50 +0200

    config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Іванночко",
+    valentineName: "Іванночко, baby girl",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
